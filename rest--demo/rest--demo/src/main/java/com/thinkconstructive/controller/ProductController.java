8206be26b18a089ae53e85e7e80293a8fa16ef6b
sarpe2715 <sarpe2715@gmail.com>
2024-08-15 20:09:12 +0530

    Bug fixing and controller update

---
+++ rest--demo/rest--demo/src/main/java/com/thinkconstructive/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.thinkconstructive.controller;
+import com.furniture.model.Product;
+import com.furniture.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/api/products")
+    public List<Product> getProducts() {
+        return productService.getAllProducts();
+    }
+}
---
+++ rest--demo/rest--demo/src/main/java/com/thinkconstructive/model/ProductService.java
@@ -0,0 +1,18 @@
+package com.thinkconstructive.model;
+
+import com.furniture.model.Product;
+import com.furniture.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
