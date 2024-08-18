629021d5c212da6952a35efb5489e74aa98874be
vishal pande <pandevishal40@gmail.com>
2024-08-18 17:24:48 +0530

    Resolved merge conflict in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import NavBar from './components/NavBar/NavBar';
-import HeroCarousel from './components/HeroCarosel/HeroCarousel';
-=======
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import NavBar from "./components/NavBar/NavBar";
 import HeroCarousel from "./components/HeroCarosel/HeroCarousel";
->>>>>>> db39bcf4fadbff6142932261c819f4e4c2c51d0d
+
 
 function App() {
   return (
