36cfdd879d2ad1f080d8dd725fc12c7466dc02d2
vishal pande <pandevishal40@gmail.com>
2024-08-18 17:40:31 +0530

    Caursoel images added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import NavBar from "./components/NavBar/NavBar";
-import HeroCarousel from "./components/HeroCarosel/HeroCarousel";
-
+import HeroCarousel from './components/HeroCarosel/HeroCarousel'
 
 function App() {
   return (
