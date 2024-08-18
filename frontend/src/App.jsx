cb678aea94cd97b4b80a1df79382e4915b560afc
ameykorde <ameykorde1@gmail.com>
2024-08-18 18:13:55 +0530

    added product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import NavBar from "./components/Navbar/Navbar";
 import HeroCarousel from "./components/HeroCarosel/HeroCarousel"
-
+import ProductPage from "./components/ProductPage/ProductPage";
 function App() {
   return (
     <>
       <NavBar />
-      <HeroCarousel />
+      {/* <HeroCarousel /> */}
+      <ProductPage/>
     </>
   );
 }
