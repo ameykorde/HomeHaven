5c28aeed5b06b2f5ae9c969725a69a67b03ea5b6
sarpe2715 <sarpe2715@gmail.com>
2024-08-18 16:05:18 +0530

    Commited project template with necessary depencencies and updated application properties

---
+++ backend/HomeHaven/src/test/java/com/HomeHaven/HomeHavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HomeHaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeHavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
